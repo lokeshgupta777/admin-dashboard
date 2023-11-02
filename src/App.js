3fc0b50b72b22e527a45c8207d5075472dac6851
lokeshgupta777 <lokesh77793@gmail.com>
2023-11-02 23:02:04 +0530

    App Initialized

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { useEffect } from 'react';
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <div className='container' id="iframe-cont"></div>
+    </div>
+  );
+}
+
+export default App;
